6122f8d3c4d2f2fd5ab9a35ef97d2093cab5e00d
Herjuno Pangestu <herjunopangestu@gmail.com>
2021-09-20 19:33:24 +0700

    wiring login and register

--- b-track/src/store/action.js
+++ b-track/src/store/action.js
@@ -1,4 +1,5 @@
-import { TOGGLE_MODAL_FORM_DETAIL, TOGGLE_MODAL_IMAGE } from "./actionType";
+import { TOGGLE_MODAL_FORM_DETAIL, TOGGLE_MODAL_IMAGE, SET_LOADING, SET_ERROR, FETCH_DEPARTMENTS } from "./actionType";
+import axios from '../apis/server'
 
 export function toggleModalFormDetail(payload) {
   return {
@@ -13,6 +14,43 @@ export function toggleModalImage(payload) {
     payload: payload,
   };
 }
+
+function setError(payload) {
+  return {
+      type : SET_ERROR,
+      payload
+  }
+}
+
+function setLoading(payload) {
+  return {
+      type : SET_LOADING,
+      payload
+  }
+}
+
+function setDepartments(payload) {
+  return {
+      type : FETCH_DEPARTMENTS,
+      payload
+  }
+}
+
+export function fetchCategories() {
+  return async function (dispatch, getState) {
+      try {
+          dispatch(setLoading(true))
+          axios.get('/departments')
+          .then((response) => {
+            dispatch(setDepartments(response.data))
+          })
+          .finally(() => dispatch(setLoading(false)))
+      } catch (error) {
+          dispatch(setError(error))
+      }
+  }
+}
+
 
 // export function fetchTes() {
 //   return async function (dispatch, getState) {
--- b-track/src/store/index.js
+++ b-track/src/store/index.js
@@ -2,7 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import reducer from "./reducer";
+import { logger } from "../middlewares/logger";
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(logger, thunk)));
 
 export default store;
