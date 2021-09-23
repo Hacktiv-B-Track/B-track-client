452c825b8d03bca049c52d79f6fbfe6081655f83
Andreas Febryanto <andreas.febryanto@gmail.com>
2021-09-23 11:24:13 +0700

    update correction

--- b-track/src/helpers/reportGenerator.js
+++ b-track/src/helpers/reportGenerator.js
@@ -1,30 +1,43 @@
 import jsPDF from "jspdf";
 import "jspdf-autotable";
-import { idrCurrency } from "./currency";
 
 import { format } from "date-fns";
+import { idrCurrency } from "./currency";
 
 const generatePDF = (data) => {
   const doc = new jsPDF();
 
-  const tableColumn = ["ID", "Name", "Amount", "Date", "Category"];
+  const tableColumn = ["Id", "Name", "Amount", "Date", "Category"];
   const tableRows = [];
 
-  data?.Transactions.forEach((transaction) => {
+  data?.Transactions.forEach((transaction, index) => {
     const transactionsData = [
-      transaction.id,
+      index + 1,
       transaction.name,
       idrCurrency(transaction.amount),
-      format(new Date(transaction.date), "yyyy-MM-dd"),
+      format(new Date(transaction.date), "d MMMM y"),
       transaction.Category.name,
     ];
+
     tableRows.push(transactionsData);
   });
+
+  const totalTransactions = [
+    "",
+    "TOTAL AMOUNT",
+    idrCurrency(data?.initial_amount - data?.amount),
+    "",
+    "",
+  ];
+
+  tableRows.push(totalTransactions);
+
   doc.text(`${data?.name} | ${idrCurrency(data?.initial_amount)}`, 14, 15);
 
   doc.autoTable(tableColumn, tableRows, { startY: 20 });
   const date = Date().split(" ");
   const dateStr = date[0] + date[1] + date[2] + date[3] + date[4];
+
   doc.save(`report_${dateStr}.pdf`);
 };
 
